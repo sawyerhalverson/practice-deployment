91bdf652494dde20a99170f0214e6005ac58a028
sawyerhalverson <shalverson321@gmail.com>
2025-01-24 12:34:42 -0700

    changed cors to only listen to frontedn

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,11 +11,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "*", // Allow all origins temporarily
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
   })
 );
 app.use(express.json());
-
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
