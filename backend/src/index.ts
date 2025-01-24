949de1d881ba319ce992486463f601891b694f3e
sawyerhalverson <shalverson321@gmail.com>
2025-01-24 12:39:16 -0700

    changed cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,8 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://practice-deployment-production.up.railway.app",
+    credentials: true,
   })
 );
 app.use(express.json());
