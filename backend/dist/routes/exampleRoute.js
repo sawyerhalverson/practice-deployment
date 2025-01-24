59593c6cdf6dc4304644686ed6c962ea2709199a
sawyerhalverson <shalverson321@gmail.com>
2025-01-24 12:32:57 -0700

    got rid of dist

--- backend/dist/routes/exampleRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/example', (req, res) => {
-    res.json({ message: 'This is an example route!' });
-});
-exports.default = router;
