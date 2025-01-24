8d0337ba78cd5cb9015318eb6eba26ef284a4665
sawyerhalverson <shalverson321@gmail.com>
2025-01-24 12:23:54 -0700

    made cors free for all

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*", // Allow all origins temporarily
   })
 );
 app.use(express.json());
