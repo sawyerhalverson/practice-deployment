6205a036fcac79c57cbd3882708e742084b90eaf
sawyerhalverson <shalverson321@gmail.com>
2025-01-27 14:29:06 -0700

    added tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-3xl font-bold text-gray-800">Basic app</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
